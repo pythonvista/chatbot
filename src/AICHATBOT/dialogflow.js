13b129a19ceefb560b6e47eead8e00d06a5acf70
Emeter Victor <93406217+pythonvista@users.noreply.github.com>
2023-05-21 18:39:29 +0100

    Update dialogflow.js

--- src/AICHATBOT/dialogflow.js
+++ src/AICHATBOT/dialogflow.js
@@ -4,35 +4,7 @@ async function ChatBot(word) {
   let sessionID = Math.floor(Math.random() * 378974206843640 + 36893787392073);
   //   let auths = gcloud auth print-access-token;
   try {
-    let res = await fetch('https://emeterr-token.onrender.com/refresh_token')
-    const data = await res.json()
-    const token = data.access_token
-    const response = await fetch(
-      `https://dialogflow.googleapis.com/v2/projects/emeterr-bff58/agent/sessions/${sessionID}:detectIntent`,
-      {
-        method: "POST",
-        mode: "cors",
-        cache: "no-cache",
-        credentials: "same-origin",
-        headers: {
-          "Content-Type": "application/json",
-          Authorization:
-            `Bearer ${token}`,
-          "x-goog-user-project": "emeterr-bff58",
-        },
-        redirect: "follow",
-        referrerPolicy: "no-referrer",
-        body: JSON.stringify({
-          query_input: {
-            text: {
-              text: word,
-              language_code: "en-US",
-            },
-          },
-        }),
-      }
-    );
-
+    const response = await fetch(`https://emeterr-ai.onrender.com/eatery/ai?word=${word}&id=${sessionID}`)
     return response;
   } catch (err) {
     console.log(err);
